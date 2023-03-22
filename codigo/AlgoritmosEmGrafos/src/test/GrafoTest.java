52752df9d6c677a1ce45e5a6674223b2190996c8
Victor Friche <104036970+friche11@users.noreply.github.com>
2023-03-22 17:12:00 -0300

    Criacao do projeto no Eclipse #1

---
+++ codigo/AlgoritmosEmGrafos/src/test/GrafoTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GrafoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ codigo/AlgoritmosEmGrafos/src/test/VerticeTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VerticeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
