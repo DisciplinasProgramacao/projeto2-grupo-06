20bca52315f02b25a2621a67bde993ef929c5a3b
luanaf4 <103442333+luanaf4@users.noreply.github.com>
2023-03-23 21:19:26 -0300

    Criação GrafoCompleto #4

--- codigo/AlgoritmosEmGrafos/src/business/GrafoCompleto.java
+++ codigo/AlgoritmosEmGrafos/src/business/GrafoCompleto.java
@@ -2,9 +2,30 @@ package business;
 
 public class GrafoCompleto extends Grafo {
 
-	public GrafoCompleto(String nome, int ordem) {
-		super(nome);
-		
-	}
+    public GrafoCompleto(String nome) {
+        super(nome);
+    }
 
+    public static Grafo grafoCompleto(int ordem) {
+        if (ordem < 1) {
+            return null;
+        }
+        GrafoCompleto grafo = new GrafoCompleto("Grafo Completo de " + ordem + " vértices");
+        for (int i = 1; i <= ordem; i++) {
+            grafo.addVertice(i);
+        }
+        for (int i = 1; i <= ordem; i++) {
+            for (int j = i + 1; j <= ordem; j++) {
+                grafo.addAresta(i, j, 1);
+            }
+        }
+        return grafo;
+    }
+
+    @Override
+    public boolean completo() {
+        int n = this.ordem();
+        return this.tamanho() == (n * (n - 1)) / 2;
+    }
+       
 }
