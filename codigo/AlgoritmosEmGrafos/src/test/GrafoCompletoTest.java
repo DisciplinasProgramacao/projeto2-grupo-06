27976d222708455ae4f38b3a8579809be9a83023
luanaf4 <103442333+luanaf4@users.noreply.github.com>
2023-03-23 21:18:21 -0300

    Teste GrafoCompleto #4

--- codigo/AlgoritmosEmGrafos/src/test/GrafoCompletoTest.java
+++ codigo/AlgoritmosEmGrafos/src/test/GrafoCompletoTest.java
@@ -1,14 +1,39 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class GrafoCompletoTest {
+import business.GrafoCompleto;
+import business.Grafo;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+public class GrafoCompletoTest {
 
+    @Test
+    public void testGrafoCompleto3Vertices() {
+        Grafo grafoCompleto = Grafo.grafoCompleto(3);
+        if (grafoCompleto != null) {
+            Assertions.assertTrue(grafoCompleto.completo());
+            Assertions.assertEquals(3, grafoCompleto.ordem());
+        }
+    }
+
+    @Test
+    public void testGrafoCompleto5Vertices() {
+        Grafo grafoCompleto = Grafo.grafoCompleto(5);
+        if (grafoCompleto != null) {
+            Assertions.assertTrue(grafoCompleto.completo());
+            Assertions.assertEquals(5, grafoCompleto.ordem());
+        }
+    }
+
+    @Test
+    public void testGrafoCompleto0Vertices() {
+        Grafo grafoCompleto = Grafo.grafoCompleto(0);
+        Assertions.assertNull(grafoCompleto);
+        if (grafoCompleto != null) {
+            Assertions.assertTrue(grafoCompleto.completo());
+            Assertions.assertEquals(0, grafoCompleto.ordem());
+        }
+
+}
 }
