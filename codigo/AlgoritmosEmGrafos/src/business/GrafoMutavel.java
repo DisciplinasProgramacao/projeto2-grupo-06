353d18e2c55e9c567b12336e9e63a9fb1b5b59b8
luanaf4 <103442333+luanaf4@users.noreply.github.com>
2023-03-27 16:58:01 -0300

    Atualização da classe GrafoMutavel #8

--- codigo/AlgoritmosEmGrafos/src/business/GrafoMutavel.java
+++ codigo/AlgoritmosEmGrafos/src/business/GrafoMutavel.java
@@ -1,45 +1,68 @@
 package business;
 
 public class GrafoMutavel extends Grafo {
-
-
-	public GrafoMutavel(String nome) {
-		super(nome);
-		
-	}
-	
-	public boolean addVertice(int id){
-        Vertice novo = new Vertice(id);
-        return this.vertices.add(id, novo);
-    }
-
-    public Vertice removeVertice(int id){
-        return null;
-    }
     
-    public boolean addAresta(int origem, int destino, int peso){
+    public GrafoMutavel(String nome) {
+        super(nome);
+    }
+
+    public boolean addVertice(int id) {
+        if (existeVertice(id) == null) {
+            Vertice novo = new Vertice(id);
+            return this.vertices.add(id, novo);
+        }
+        return false;
+    }
+
+    public Vertice removeVertice(int id) {
+        Vertice vertice = existeVertice(id);
+        if (vertice != null) {
+            for (Integer vizinho : vertice.vizinhos()) {
+                Vertice v = existeVertice(vizinho);
+                if (v != null) {
+                    v.removeAresta(id);
+                }
+            }
+            this.vertices.remove(id);
+        }
+        return vertice;
+    }
+
+    public Vertice existeVertice(int idVertice) {
+        return this.vertices.find(idVertice);
+    }
+
+    public boolean addAresta(int origem, int destino, int peso) {
         boolean adicionou = false;
         Vertice saida = this.existeVertice(origem);
         Vertice chegada = this.existeVertice(destino);
-        if(saida!=null && chegada !=null){
-            adicionou = (saida.addAresta(destino, peso)&&chegada.addAresta(origem, peso));
+        if (saida != null && chegada != null) {
+            adicionou = (saida.addAresta(destino, peso) && chegada.addAresta(origem, peso));
         }
         return adicionou;
-
     }
 
-
-    public Aresta removeAresta(int origem, int destino){
+    public Aresta removeAresta(int origem, int destino) {
+        Vertice saida = this.existeVertice(origem);
+        if (saida != null) {
+            Aresta removida = saida.removeAresta(destino);
+            if (removida != null) {
+                Vertice chegada = this.existeVertice(destino);
+                if (chegada != null) {
+                    chegada.removeAresta(origem);
+                }
+            }
+            return removida;
+        }
         return null;
     }
-    
-    public void carregar(String nomeArquivo){
 
+    public Aresta existeAresta(int verticeA, int verticeB) {
+        Vertice vA = this.existeVertice(verticeA);
+        if (vA != null) {
+            return vA.existeAresta(verticeB);
+        }
+        return null;
     }
-
-    public void salvar(String nomeArquivo){
-        
-    }
-
 
 }
