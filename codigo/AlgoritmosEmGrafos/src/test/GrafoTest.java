8d9cd6b184a62b1a7db69d1471e7ff5adc91ff21
Victor Friche <104036970+friche11@users.noreply.github.com>
2023-03-27 18:22:14 -0300

    Criacao do teste para salvar um grafo #3

--- codigo/AlgoritmosEmGrafos/src/test/GrafoTest.java
+++ codigo/AlgoritmosEmGrafos/src/test/GrafoTest.java
@@ -7,8 +7,15 @@ import org.junit.jupiter.api.Test;
 class GrafoTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	    public void testSalvarGrafo() {
+		Grafo grafo = new Grafo("teste");
+		grafo.addVertice(1);
+        	grafo.addVertice(2);
+        	grafo.addVertice(3);
+        	grafo.addAresta(1, 2, 3);
+	    	grafo.salvar("teste.txt");
+	    
+	    }
+
 
 }
