efcb2790469073ad1b7425c714b737762acc97fd
Victor Friche <104036970+friche11@users.noreply.github.com>
2023-03-27 20:05:15 -0300

    Atualizacao teste GrafoDirecionado #9

--- codigo/AlgoritmosEmGrafos/src/test/GrafoDirecionadoTest.java
+++ codigo/AlgoritmosEmGrafos/src/test/GrafoDirecionadoTest.java
@@ -4,11 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import business.GrafoDirecionado;
+
 class GrafoDirecionadoTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	void testCriarGrafoDirecionado() {
+		GrafoDirecionado grafo = new GrafoDirecionado("teste");
+		grafo.addVertice(1);
+		grafo.addVertice(2);
+		grafo.addVertice(3);
+		grafo.addVertice(4);
+		
+		assertTrue(grafo.addAresta(1, 2, 0));
+		assertFalse(grafo.addAresta(2,1,0));    //Ja existe aresta de 1 para 2, entao ele nao adiciona outra aresta de 2 para 1
+		assertNull(grafo.existeAresta(1,2));
+		assertNull(grafo.existeAresta(2,1));
 
 }
