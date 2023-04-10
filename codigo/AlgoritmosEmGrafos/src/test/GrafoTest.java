3751947cd157559f9459479b3c1d910e2ac61ea3
luanaf4 <103442333+luanaf4@users.noreply.github.com>
2023-04-10 18:39:42 -0300

    Criação teste busca em largura #10

--- codigo/AlgoritmosEmGrafos/src/test/GrafoTest.java
+++ codigo/AlgoritmosEmGrafos/src/test/GrafoTest.java
@@ -17,6 +17,40 @@ class GrafoTest {
 	    	grafo.salvar("teste.txt");
 	    
 	    }
+	
+	 @Test
+	    public void testBfs() {
+	        // Grafo de entrada
+	        Grafo grafoEntrada = new GrafoNaoDirecionado("grafoEntrada");
+	        grafoEntrada.addVertice(1);
+	        grafoEntrada.addVertice(2);
+	        grafoEntrada.addVertice(3);
+	        grafoEntrada.addVertice(4);
+	        grafoEntrada.addVertice(5);
+	        grafoEntrada.addAresta(1, 2, 1);
+	        grafoEntrada.addAresta(1, 3, 1);
+	        grafoEntrada.addAresta(2, 4, 1);
+	        grafoEntrada.addAresta(3, 4, 1);
+	        grafoEntrada.addAresta(4, 5, 1);
 
+	        // Grafo de saída esperado
+	        Grafo grafoSaidaEsperado = new GrafoNaoDirecionado("grafoSaidaEsperado");
+	        grafoSaidaEsperado.addVertice(1);
+	        grafoSaidaEsperado.addVertice(2);
+	        grafoSaidaEsperado.addVertice(3);
+	        grafoSaidaEsperado.addVertice(4);
+	        grafoSaidaEsperado.addVertice(5);
+	        grafoSaidaEsperado.addAresta(1, 2, 1);
+	        grafoSaidaEsperado.addAresta(1, 3, 1);
+	        grafoSaidaEsperado.addAresta(2, 4, 1);
+	        grafoSaidaEsperado.addAresta(3, 4, 1);
+	        grafoSaidaEsperado.addAresta(4, 5, 1);
+
+	        // Executa o método bfs com o grafo de entrada
+	        Grafo grafoSaida = grafoEntrada.bfs(1);
+
+	        // Verifica se o grafo de saída é igual ao grafo esperado
+	        assertEquals(grafoSaidaEsperado, grafoSaida);
+	    }
 
 }
