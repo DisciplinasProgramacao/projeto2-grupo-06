ece25a917dc43028c16c7d7ff30581609d44ea23
gscantoni <104282001+gscantoni@users.noreply.github.com>
2023-04-10 18:39:31 -0300

    Grafo Ponderado

---
+++ codigo/AlgoritmosEmGrafos/src/business/GrafoPonderado.java
@@ -0,0 +1,35 @@
+public class GrafoPonderado {
+	private final int MAX_VERTICE = 10;
+	private Vertice[] Vertices;
+    
+    private Aresta[][] matrixDeAdjacencia;
+    private int quantidadeAtualDeVertices;
+    
+    public GrafoPonderado(){
+        this.Vertices = new Vertice[MAX_VERTICE];
+        matrixDeAdjacencia = new Aresta[MAX_VERTICE][MAX_VERTICE];
+		quantidadeAtualDeVertices = 0;
+    }
+    
+    public void addVertice(char rotulo){
+		Vertices[quantidadeAtualDeVertices++] = new Vertice(rotulo);
+	}
+
+	public void addArestaNaoOrientada(int inicio, int fim, int peso){
+        Vertice origem = Vertices[inicio];
+        Vertice destino = Vertices[fim];
+		matrixDeAdjacencia[inicio][fim] = new Aresta(origem, destino, peso);
+		matrixDeAdjacencia[fim][inicio] = new Aresta(destino, origem, peso);
+	}
+
+	public void MostraGrafo(){
+		for(int i=0; i<matrixDeAdjacencia.length; i++){
+			for(int j=0; j<matrixDeAdjacencia[i].length; j++){
+				Aresta Aresta = matrixDeAdjacencia[i][j];
+				if(Aresta != null){
+					System.out.println(Aresta.getorigem().getrotulo() + " - " + Aresta.getdestino().getrotulo() + ": " + Aresta.getpeso());
+				}
+			}
+		}
+	}
+}
