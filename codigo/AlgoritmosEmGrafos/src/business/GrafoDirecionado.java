02f232732491b2c5db0aaa27884b30e336afe25f
friche11 <vicfriche@gmail.com>
2023-04-11 23:15:58 -0300

    Atualizacao direcionado e nao direcionado #9

--- codigo/AlgoritmosEmGrafos/src/business/GrafoDirecionado.java
+++ codigo/AlgoritmosEmGrafos/src/business/GrafoDirecionado.java
@@ -7,11 +7,14 @@ public class GrafoDirecionado extends GrafoMutavel{
 
 	}
 	
+	// Grafo direcionado possui aresta ponderada e deve ter peso maior que 0.
+	
 	public boolean addAresta(int origem, int destino, int peso){
+		
         boolean adicionou = false;
         Vertice saida = this.existeVertice(origem);
         Vertice chegada = this.existeVertice(destino);
-        if(saida!=null && chegada !=null){
+        if(saida!=null && chegada !=null && peso>0){
             adicionou = (saida.addAresta(destino, peso)&&chegada.addAresta(origem, peso));
         }
         return adicionou;
@@ -20,7 +23,18 @@ public class GrafoDirecionado extends GrafoMutavel{
 
 
     public Aresta removeAresta(int origem, int destino){
-        return null;
+    	 Vertice saida = this.existeVertice(origem);
+         if (saida != null) {
+             Aresta removida = saida.removeAresta(destino);
+             if (removida != null) {
+                 Vertice chegada = this.existeVertice(destino);
+                 if (chegada != null) {
+                     chegada.removeAresta(origem);
+                 }
+             }
+             return removida;
+         }
+         return null;
     }
 	
 
--- codigo/AlgoritmosEmGrafos/src/business/GrafoNaoDirecionado.java
+++ codigo/AlgoritmosEmGrafos/src/business/GrafoNaoDirecionado.java
@@ -7,12 +7,14 @@ public class GrafoNaoDirecionado extends GrafoMutavel{
 	
 	}
 	
-	public boolean addAresta(int origem, int destino, int peso){
+	//Consideramos que um grafo direcionado nao possui aresta ponderada para diferenciar grafo direcionado e nao direcionado.
+	
+	public boolean addAresta(int origem, int destino){
         boolean adicionou = false;
         Vertice saida = this.existeVertice(origem);
         Vertice chegada = this.existeVertice(destino);
         if(saida!=null && chegada !=null){
-            adicionou = (saida.addAresta(destino, peso)&&chegada.addAresta(origem, peso));
+            adicionou = (saida.addAresta(destino)&&chegada.addAresta(origem));
         }
         return adicionou;
 
@@ -20,7 +22,18 @@ public class GrafoNaoDirecionado extends GrafoMutavel{
 
 
     public Aresta removeAresta(int origem, int destino){
-        return null;
+    	 Vertice saida = this.existeVertice(origem);
+         if (saida != null) {
+             Aresta removida = saida.removeAresta(destino);
+             if (removida != null) {
+                 Vertice chegada = this.existeVertice(destino);
+                 if (chegada != null) {
+                     chegada.removeAresta(origem);
+                 }
+             }
+             return removida;
+         }
+         return null;
     }
 
 }
